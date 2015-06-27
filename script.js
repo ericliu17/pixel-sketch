4d77c3d4709c02776ea807c2ebcf7bee1b8d1dfb
supersymme3 <circlethesquare@icloud.com>
2015-06-27 16:58:45 -0700

    v1.00

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    var cells = "";
+	var cellSize = 5;
+	var height = Math.floor(($(window).height() - 1)/(cellSize + 1)) - 35;	//Subtract header
+	var width = Math.floor(($(window).width() - 1)/(cellSize + 1));
+
+    function makeGrid(x, y) {
+    	for (var i = 0; i < x; i++) {
+    		cells += "<tr>";
+    		for (var j = 0; j < y; j++) {
+    			cells += "<td></td>";
+    		};
+    		cells += "</tr>";
+    	};
+    	return cells;
+    };
+
+    $("table").append(makeGrid(height, width));
+
+    $(window).resize(function() {
+    	$("table").empty();
+ 		$("table").append(makeGrid(height, width));
+	});
+
+	$("td").hover(function() {
+		$(this).css("background-color", "blue");
+	});
+
+	$("button").click(function() {
+		location.reload();
+	});
+
+});
