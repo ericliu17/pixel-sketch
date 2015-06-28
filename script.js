76414b8e715f4be6eb7b12f426f91c850765b91f
supersymme3 <circlethesquare@icloud.com>
2015-06-27 17:30:51 -0700

    v1.10 - Added instructions, changed border styling, optimized auto-resize

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-    var cells = "";
 	var cellSize = 5;
 	var height = Math.floor(($(window).height() - 1)/(cellSize + 1)) - 35;	//Subtract header
 	var width = Math.floor(($(window).width() - 1)/(cellSize + 1));
 
     function makeGrid(x, y) {
+        var cells = "";
     	for (var i = 0; i < x; i++) {
     		cells += "<tr>";
     		for (var j = 0; j < y; j++) {
