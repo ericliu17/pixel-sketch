be735801d94297a94bc74f53dac86772aa4d8316
supersymme3 <circlethesquare@icloud.com>
2015-07-03 17:07:59 -0700

    v1.20 - Added colorpicker, animated logo, random initial drawing color. Updated instructions.

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 $(document).ready(function () {
 
-	var cellSize = 5;
-	var height = Math.floor(($(window).height() - 1)/(cellSize + 1)) - 35;	//Subtract header
-	var width = Math.floor(($(window).width() - 1)/(cellSize + 1));
+    var rainbow = ["violet", "blue", "green", "yellow", "orange", "red"];   
+    var $pixel = $("#name span"), firstName = "pixel", char = 0;            //For animating logo                                                  
+    var cellSize = 5,                                                       //For making grid
+        height = Math.floor(($(window).height() - 1)/(cellSize + 1)) - 40,      //Subtract header
+        width = Math.floor(($(window).width() - 1)/(cellSize + 1));
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    function typeFirstName() {
+        var initialColor = rainbow[getRandomInt(0, rainbow.length)];
+
+        var timeout = setTimeout(function() {
+            var letters = firstName.substring(0, char);
+
+            $pixel.text(letters).css("color", initialColor);
+
+            if (char === firstName.length) {
+                $("td").hover(function() {                                  //Set random initial drawing color                              
+                    $(this).css("background-color", initialColor);
+                });
+                clearTimeout(timeout);
+            }
+            else {
+                char++;
+                typeFirstName();
+            };
+
+        }, 300);
+    };
 
     function makeGrid(x, y) {
         var cells = "";
+
     	for (var i = 0; i < x; i++) {
     		cells += "<tr>";
     		for (var j = 0; j < y; j++) {
@@ -13,21 +42,41 @@ $(document).ready(function () {
     		};
     		cells += "</tr>";
     	};
+
     	return cells;
     };
 
-    $("table").append(makeGrid(height, width));
+    function makeColorPicker() {
+        var buttons = "";
 
-    $(window).resize(function() {
-    	$("table").empty();
- 		$("table").append(makeGrid(height, width));
-	});
+        $.each(rainbow, function(index, color) {
+            buttons += "<button id=\"" + color + "\" style=\"background-color:" + 
+                        color + "\"></button>";
+        });
 
-	$("td").hover(function() {
-		$(this).css("background-color", "blue");
-	});
+        return buttons;
+    }
 
-	$("button").click(function() {
+    $(window).load(function() {                                             //Animate logo on load
+        typeFirstName();
+    });
+
+    $(window).resize(function() {                                           //Redraw grid on window resize
+        location.reload();
+    });
+
+    $("#colorpicker").append(makeColorPicker());                            //Make colorPicker      
+
+    $("#palette").append(makeGrid(height, width));                          //Make grid
+
+    $("#colorpicker button").click(function() {                             //Colorpicker controls
+        var color = $(this).attr("id");
+        $("td").hover(function() {
+            $(this).css("background-color", color);
+        });
+    });
+
+	$("#clear").click(function() {                                          //Clear grid button
 		location.reload();
 	});
 
